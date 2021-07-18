3217c72dd325f5a50b7820f069d426d1d3b4d84d
dbcrea <djelloul.be@gmail.com>
2021-07-18 20:25:27 +0200

    adding fullTeam notification

--- src/app/menu/menu/menu.component.ts
+++ src/app/menu/menu/menu.component.ts
@@ -10,7 +10,7 @@ import { JoueurService } from 'src/app/services/joueur.service';
 export class MenuComponent implements OnInit {
   joueurs?: Joueur[];
   displayAddButton = true;
-
+  fullTeam = false;
   constructor(private joueurService: JoueurService) { }
 
   ngOnInit(): void {
@@ -18,6 +18,7 @@ export class MenuComponent implements OnInit {
       this.joueurs = data;
     if (this.joueurs.length >= 23){
       this.displayAddButton = false; 
+      this.fullTeam = true;
     }
     
     });
